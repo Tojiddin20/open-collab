ed6501cf2958330538ac8c3ca67eaf3609f47a2d
SaidkhojaIftikhor <saidkhojaiftikhor2002@gmail.com>
2023-10-07 17:23:48 +0500

    add tailwindcss and home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to OpenCollab</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="font-sans bg-gray-100">
+
+<header class="bg-gray-800 text-white py-10 text-center">
+    <h1 class="text-4xl font-bold">Welcome to OpenCollab</h1>
+    <p class="mt-2">Your Gateway to Open Science Collaborations</p>
+</header>
+
+<section class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 my-10 p-8 bg-white shadow-md rounded">
+    <div>
+        <h2 class="text-2xl font-bold mb-4">What is OpenCollab?</h2>
+        <p class="text-gray-700">OpenCollab is a vibrant community connecting project creators with skilled collaborators in the open science domain. Find exciting projects or contribute your expertise to advance scientific research.</p>
+    </div>
+    <div>
+        <img src="https://placekitten.com/600/400" alt="Collaboration Image" class="rounded-md shadow-md">
+    </div>
+</section>
+
+<section class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 my-10 p-8 bg-white shadow-md rounded">
+    <div>
+        <img src="https://placekitten.com/600/400" alt="Project Image" class="rounded-md shadow-md">
+    </div>
+    <div>
+        <h2 class="text-2xl font-bold mb-4">How it Works</h2>
+        <p class="text-gray-700">Project creators post their open science projects, specifying required skills. Collaborators explore, connect, and contribute to projects aligned with their expertise. Join OpenCollab and be part of the scientific collaboration revolution!</p>
+    </div>
+</section>
+
+<section class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 my-10 p-8 bg-white shadow-md rounded">
+    <div>
+        <h2 class="text-2xl font-bold mb-4">Get Started</h2>
+        <p class="text-gray-700">Ready to embark on a collaborative scientific journey? Join OpenCollab now and start exploring projects or showcase your own. Let's build the future of science together!</p>
+        <a href="#" class="mt-4 inline-block bg-gray-800 text-white py-2 px-4 rounded-md">Join OpenCollab</a>
+    </div>
+    <div>
+        <img src="https://placekitten.com/600/400" alt="Getting Started Image" class="rounded-md shadow-md">
+    </div>
+</section>
+
+</body>
+</html>
