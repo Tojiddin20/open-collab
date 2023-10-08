31d4a5ee94869e321781439954db7686d293c2c6
Shekhov Ismoil <veyxov@gmail.com>
2023-10-08 15:11:18 +0500

    created registration and database

--- UI/opencollab/back-end/openCollab/Controllers/DataContext.cs
+++ UI/opencollab/back-end/openCollab/Controllers/DataContext.cs
@@ -1,6 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 
-public class DataContext : DbContext {
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get;  set; }
+    public string Password { get;  set; }
+}
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public int UserId { get; set; }
+    public virtual User User { get; set; } = null!;
+}
+
+
+public class DataContext : DbContext
+{
     public DbSet<User> Users { get; set; }
     public DbSet<Project> Projects { get; set; }
 
--- UI/opencollab/back-end/openCollab/Controllers/WeatherForecastController.cs
+++ UI/opencollab/back-end/openCollab/Controllers/WeatherForecastController.cs
@@ -11,13 +11,33 @@ public class WeatherForecastController : ControllerBase
     [HttpPost("reg")]
     public IActionResult Register(RegisterDto dto)
     {
+        var user = new User
+        {
+            Name = dto.Name,
+            Email = dto.Email,
+            Password = dto.Password
+        };
 
+        Context.Users.Add(user);
+        Context.SaveChanges();
+
+        return Ok(user);
     }
 
-    [HttpGet("")]
-    public IActionResult CreateProject()
+    [HttpGet("project/create")]
+    public IActionResult CreateProject(ProjectDto dto)
     {
+        var project = new Project
+        {
+            Name = dto.Name,
+            Description = dto.Description,
+            UserId = dto.UserId
+        };
 
+        Context.Projects.Add(project);
+        Context.SaveChanges();
+
+        return Ok(project);
     }
 }
 
@@ -27,4 +47,10 @@ public class RegisterDto
     public string Password { get; set; }
     public string Name { get; set; }
     public IFormFile Avatar { get; set; }
+}
+
+public class ProjectDto {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int UserId { get; set; }
 }
