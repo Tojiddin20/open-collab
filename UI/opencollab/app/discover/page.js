7bb2d38ab10d180ca59f778e50925b1424176eba
Shekhov Ismoil <veyxov@gmail.com>
2023-10-08 12:51:01 +0500

    make the card

--- UI/opencollab/app/discover/page.js
+++ UI/opencollab/app/discover/page.js
@@ -2,19 +2,19 @@
 
 // Import necessary modules from 'react'
 import { useState } from 'react';
-import { useSprings, animated, interpolate } from 'react-spring';
+import { useSprings, animated, to } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
 
 // Define the cards array
 const cards = [
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg/1024px-Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg/1024px-Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg',
 ];
 
 // Define the initial state
 const initialState = {
-  x: 0,
-  y: 0,
-  rot: 0,
+    x: 0,
+    y: 0,
+    rot: 0,
 };
 
 function handleSwipe(dir) {
@@ -27,57 +27,75 @@ function handleSwipe(dir) {
 
 // Define the Deck component
 const Deck = () => {
-  const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
+    const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
 
-  const [springs, set] = useSprings(cards.length, (i) => ({
-    from: initialState,
-    ...initialState,
-  }));
+    const [springs, set] = useSprings(cards.length, (i) => ({
+        from: initialState,
+        ...initialState,
+    }));
 
-  const bindGesture = useDrag(({ args: [index], down, movement: [deltaX, deltaY], distance, velocity }) => {
-    const trigger = velocity > 0.2 || Math.abs(deltaX) > 100;
-    const dir = deltaX > 0 ? 1 : -1;
-    if (!down && trigger) {
-        gone.add(index);
+    const bindGesture = useDrag(({ args: [index], down, movement: [deltaX, deltaY], distance, velocity }) => {
+        const trigger = velocity > 0.2 || Math.abs(deltaX) > 100;
+        const dir = deltaX > 0 ? 1 : -1;
+        if (!down && trigger) {
+            gone.add(index);
 
-        console.log(dir);
-        handleSwipe(dir);
-    }
+            console.log(dir);
+            handleSwipe(dir);
+        }
 
-    set((i) => {
-      if (index !== i) return;
-      const isGone = gone.has(index);
+        set((i) => {
+            if (index !== i) return;
+            const isGone = gone.has(index);
 
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? deltaX : 0;
-      const y = down ? deltaY : 0;
-      const rot = down ? deltaX / 10 + (isGone ? dir * 10 * velocity : 0) : 0;
-      const config = {
-        friction: 50,
-        tension: down ? 800 : isGone ? 200 : 500,
-        clamp: down,
-      };
+            const x = isGone ? (200 + window.innerWidth) * dir : down ? deltaX : 0;
+            const y = down ? deltaY : 0;
+            const rot = down ? deltaX / 10 + (isGone ? dir * 10 * velocity : 0) : 0;
+            const config = {
+                friction: 50,
+                tension: down ? 800 : isGone ? 200 : 500,
+                clamp: down,
+            };
 
-      return { x, y, rot, config };
+            return { x, y, rot, config };
+        });
     });
-  });
 
-  return (
-    <div className="flex justify-center">
-      {springs.map(({ x, y, rot }, i) => (
-        <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
-          <animated.div
-            {...bindGesture(i)}
-            style={{
-              transform: interpolate([rot], (rot) => `rotateX(30deg) rotateY(${rot / 10}deg) rotateZ(${rot}deg)`),
-              backgroundImage: `url(${cards[i]})`,
-              width: '200px',
-              height: '300px', // Adjust card size as needed
-            }}
-          />
-        </animated.div>
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex justify-center">
+            <div className="w-1/2 h-full select-none bg-red-400">
+                {springs.map(({ x, y, rot }, i) => (
+                    <animated.div key={i} style={{ transform: to([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
+                        <animated.div
+                            {...bindGesture(i)}
+                            style={{
+                                transform: to([rot], (rot) => `rotateX(30deg) rotateY(${rot / 10}deg) rotateZ(${rot}deg)`),
+                                backgroundSize: 'cover',
+                            }}
+                        >
+                            <div className="flex justify-center">
+                                <div className="w-3/4 bg-white p-8 rounded shadow-lg">
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg/1024px-Barack_Obama_with_artistic_gymnastic_McKayla_Maroney_2.jpg"
+                                        alt="Project Banner"
+                                        className="w-full mb-4 rounded-lg select-none"
+                                        draggable="false"
+                                        dragstart="false"
+                                    />
+                                    <h2 className="text-xl font-bold mb-2">Project Name</h2>
+                                    <p className="text-gray-700 mb-4">Creator Name</p>
+                                    <p className="text-gray-800">
+                                        Project DescriptionLorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </div>
+                        </animated.div>
+                    </animated.div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Deck;
