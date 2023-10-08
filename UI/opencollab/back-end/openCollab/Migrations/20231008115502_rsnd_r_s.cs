accbf8305e658dd3a6423a8d7d54925965bac9e7
Shekhov Ismoil <veyxov@gmail.com>
2023-10-08 16:55:17 +0500

    migrations!

---
+++ UI/opencollab/back-end/openCollab/Migrations/20231008115502_rsnd_r_s.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace openCollab.Migrations
+{
+    /// <inheritdoc />
+    public partial class rsnd_r_s : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserReview",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProjectId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Approved = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserReview", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserReview_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserReview_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserReview_ProjectId",
+                table: "UserReview",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserReview_UserId",
+                table: "UserReview",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserReview");
+        }
+    }
+}
